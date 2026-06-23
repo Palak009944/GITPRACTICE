22768255afcc352a0fceb38e2909415aa18fb782
Palak009944 <palaksobti2406@gmail.com>
2026-06-23 10:46:45 +0530

    Added example of (1) INHERITANCE

--- pracprac.py
+++ pracprac.py
@@ -11,3 +11,21 @@ dog3=Dog("Briar",6)
 print(dog1.species)
 print(dog2.species)
 print(dog3.name,dog3.age)
+
+#Example of inheritance 
+
+class Animal:
+    def eat(self):
+        print("Eating")
+    def sleep(self):
+        print("Sleeping")
+
+class cat(Animal):
+    pass
+
+class cow(Animal):
+    pass 
+c1=cat()
+c1.eat()
+m1=cow()
+m1.sleep()
