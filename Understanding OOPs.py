649219de1961c7ca49056368460319284edf5d34
Palak009944 <palaksobti2406@gmail.com>
2026-06-25 11:41:27 +0530

    A simple program using OOPs

---
+++ Understanding OOPs.py
@@ -0,0 +1,21 @@
+class Car:
+    def __init__(self,brand,model,year):
+        self.brand=brand
+        self.model=model
+        self.year=year
+    
+    def introduce(self):
+        print("Hello!")
+        print("I am",self.brand)
+        print("I was manufactured in",self.year)
+    
+    def start_engine(self):
+        print(self.brand,self.model,"Engine started!")
+
+
+c1=Car("Toyota","Corolla",2022)
+c2=Car("BMW","X5",2024)
+c1.introduce()
+c2.introduce()
+c1.start_engine()
+c2.start_engine()
